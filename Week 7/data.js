56cd84751f05d4ec24c323dbcba7f3f9419b28ae
Manda Akshaya <akshayamanda1310@gmail.com>
2025-09-22 11:25:13 +0530

    Update data.js

--- Week 7/data.js
+++ Week 7/data.js
@@ -1,6 +1,7 @@
 export const books = [
-  { title: "JavaScript Essentials", author: "John Doe", price: 499, availability: "in stock" },
-  { title: "Learn React", author: "Jane Smith", price: 699, availability: "in stock" },
-  { title: "Node.js in Action", author: "Mark Lee", price: 599, availability: "out of stock" },
-  { title: "Python for Everyone", author: "Ramya Krishna", price: 450, availability: "in stock" }
+  { title: "OOPA", author: "Akshaya", price: 1499, availability: "in stock" },
+  { title: "FEDF", author: "Madhumitha", price: 1699, availability: "in stock" },
+  { title: "DBMS", author: "Akshara", price: 1599, availability: "out of stock" },
+  { title: "OS", author: "Lahari", price: 1450, availability: "in stock" }
 ];
+

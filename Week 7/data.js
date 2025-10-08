09c069519b99ef2bf49cd37f1acaa013daff27c8
Manda Akshaya <akshayamanda1310@gmail.com>
2025-10-08 08:42:09 +0530

    Add files via upload

---
+++ Week 7/data.js
@@ -0,0 +1,6 @@
+export const books = [
+  { title: "OOPA", author: "Akshaya", price: 1499, availability: "in stock" },
+  { title: "FEDF", author: "Madhumitha", price: 1699, availability: "in stock" },
+  { title: "DBMS", author: "Akshara", price: 1599, availability: "out of stock" },
+  { title: "OS", author: "Lahari", price: 1450, availability: "in stock" }
+];
